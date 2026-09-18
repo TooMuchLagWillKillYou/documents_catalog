57a7188f76e18ca848dd39a857728a512132d3fe
tooMuchLagWillKillYou <diego.bar@live.it>
2026-09-18 10:42:01 +0200

    reading csv and txt

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 import os
 import pypdf
+import csv
 from pathlib import Path
 from dataclasses import dataclass, field
 
@@ -12,23 +13,30 @@ class Document:
     metadata: dict = field(default_factory=dict)
 
 def _load_pdf(path: str) -> list[Document]:
-    reader = pypdf.PdfReader(path)
 
+    reader = pypdf.PdfReader(path)
     pages = []
     
     for i, page in enumerate(reader.pages):
         text = page.extract_text()
-        print(f"\t\tText: {text}")
         pages.append(
             Document(text, {"source": str(path), "page": i + 1, "type": "pdf"})
         )
     return pages
 
 def _load_txt(path: str) -> Document:
-    pass
+    with open(path, encoding="utf-8") as f:
+        return [Document(f.read(), {"source": str(path), "type": "txt"})]
 
 def _load_csv(path: str) -> Document:
-    pass
+    with open(path, newline='') as f:
+        reader = csv.reader(f, delimiter=" ", quotechar="|")
+        result = ""
+        for r in reader:
+            result += ", ".join(r)
+
+        print(result)
+        return [Document(result, {"source": path, "type": "csv"})]
 
 def load_documents() -> list[Document]:
 
@@ -41,9 +49,11 @@ def load_documents() -> list[Document]:
         extension = Path(f.path).suffix.lower()
 
         if extension == ".pdf":
-            docs.extend(_load_pdf(f.path))
+            # docs.extend(_load_pdf(f.path))
+            pass
         elif extension == ".txt":
-            docs.append(_load_txt(f.path))
+            # docs.append(_load_txt(f.path))
+            pass
         elif extension == ".csv":
             docs.append(_load_csv(f.path))
         else:
